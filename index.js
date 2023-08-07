9921d935d9f62736f2218bc129f09d3f25be6306
Juan David <judaram98@gmail.com>
2023-08-07 15:33:33 -0600

    Query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'); //Se llama el modulo express
+const faker = require('faker');
 const app = express(); //Se crea la app con express
 const port = 3000; //Se selecciona el puerto en el cual desplegar la app
 
@@ -10,16 +11,17 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta o nuevo endpoint');
 });
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'producrto 1',
-      price: 1000,
-    },
-    {
-      name: 'producrto 2',
-      price: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
@@ -29,12 +31,24 @@ app.get('/products/:id', (req, res) => {
     price: 2000,
   });
 });
+
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
   res.json({
     categoryId,
     productId,
   });
+});
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
 });
 
 //Ponemos nuestra app a escuchar en caso de que exista una conexion
