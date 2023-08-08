7bbfc2bc9bdd1661138ba3d72d28909b3042f1af
Juan David <judaram98@gmail.com>
2023-08-08 08:41:09 -0600

    Whitelist for cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'); //Se llama el modulo express
+const cors = require('cors');
 const routerApi = require('./routes');
 const {
   logErrors,
@@ -9,6 +10,18 @@ const app = express(); //Se crea la app con express
 const port = 3000; //Se selecciona el puerto en el cual desplegar la app
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no Permitido'));
+    }
+  },
+};
+app.use(cors(options));
 
 //Ahora se envia una respuesta por emdio de un callback
 // app.get('/', (req, res) => {
